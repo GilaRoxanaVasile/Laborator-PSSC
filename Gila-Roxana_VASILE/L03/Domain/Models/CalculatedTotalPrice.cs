bae47e652289a11ff09df1710eb477e79b5ceac5
Gila-Roxana Vasile <vasile.roxana28@gmail.com>
2022-10-23 10:27:43 +0300

    innebunesc

---
+++ Gila-Roxana_VASILE/L03/Domain/Models/CalculatedTotalPrice.cs
@@ -0,0 +1,13 @@
+﻿using L02_PSSC.Domain;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static L02_PSSC.Domain.Quantity;
+
+namespace Domain.Models
+{
+    public record CalculatedTotalPrice(Client client, Guid idCart, ProductCode productCode, IQuantity quantity, decimal totalPrice);
+}
