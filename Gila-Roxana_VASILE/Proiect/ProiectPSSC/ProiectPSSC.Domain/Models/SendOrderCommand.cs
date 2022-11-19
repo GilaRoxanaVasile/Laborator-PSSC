654e016b08f0538bbfeed6688ca7f20fd68a9999
GilaRoxanaVasile <vasile.roxana28@gmail.com>
2022-11-20 00:19:15 +0200

    incepProiect

---
+++ Gila-Roxana_VASILE/Proiect/ProiectPSSC/ProiectPSSC.Domain/Models/SendOrderCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProiectPSSC.Domain.Models
+{
+    public record SendOrderCommand
+    {
+        public SendOrderCommand(IReadOnlyCollection<UnvalidatedClientOrder> inputClientProducts)
+        {
+            InputClientProducts = inputClientProducts;
+        }
+        public IReadOnlyCollection<UnvalidatedClientOrder> InputClientProducts { get; }
+    }
+}
---
+++ Gila-Roxana_VASILE/Proiect/ProiectPSSC/ProiectPSSC.Domain/PlaceOrderWorkflow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LanguageExt.Prelude;
+using LanguageExt;
+using ProiectPSSC.Domain.Models;
+using static ProiectPSSC.Domain.Models.OrderProducts;
+using ProiectPSSC.Domain.Models;
+using System.Data;
+using Microsoft.Extensions.Logging;
+
+namespace ProiectPSSC.Domain
+{
+    public static class PlaceOrderWorkflow
+    {
+
+    }
+}
