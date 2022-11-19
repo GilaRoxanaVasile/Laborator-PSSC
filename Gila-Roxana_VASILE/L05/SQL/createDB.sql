e8962db05935658b6370597664ddb975c8cb000c
GilaRoxanaVasile <vasile.roxana28@gmail.com>
2022-11-20 00:16:44 +0200

    incepProiect

--- Gila-Roxana_VASILE/L05/SQL/createDB.sql
+++ Gila-Roxana_VASILE/L05/SQL/createDB.sql
@@ -1,7 +1,9 @@
+
 CREATE TABLE [dbo].[Product](
 	[ProductId] [int] IDENTITY(1,1) NOT NULL,
 	[Code] [varchar](7) NOT NULL,
 	[Stoc] [int] NOT NULL,
+	[Pret] [decimal] NOT NULL,
  CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
 (
 	[ProductId] ASC
@@ -9,10 +11,25 @@ CREATE TABLE [dbo].[Product](
 ) ON [PRIMARY]
 GO
 
+CREATE TABLE [dbo].[Client](
+	[ClientId] [int] IDENTITY(1,1) NOT NULL,
+	[Email] [varchar](20) NOT NULL,
+	[Name] [varchar](30) NOT NULL,
+	[PhoneNumber] [varchar](15) NOT NULL,
+ CONSTRAINT [PK_Client] PRIMARY KEY CLUSTERED 
+(
+	[ClientId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO	
+
+
 CREATE TABLE [dbo].[OrderHeader](
 	[OrderId] [int] IDENTITY(1,1) NOT NULL,
-	[Address] [varchar](50) NOT NULL,
-	[Total] [decimal] NOT NULL,
+	[ClientId] [int] NOT NULL,
+	[TotalPrice] [decimal] NOT NULL,
+	[PaymentOption] [varchar](20) NOT NULL,
+	[CardDetails] [varchar](50), 
  CONSTRAINT [PK_OrderHeader] PRIMARY KEY CLUSTERED 
 (
 	[OrderId] ASC
@@ -22,9 +39,9 @@ GO
 
 CREATE TABLE [dbo].[OrderLine](
 	[OrderLineId] [int] IDENTITY(1,1) NOT NULL,
+	[OrderId] [int] NOT NULL,
 	[ProductId] [int] NOT NULL,
 	[Quantity] [decimal] NULL,
-	[Price] [decimal] NULL,
  CONSTRAINT [PK_OrderLine] PRIMARY KEY CLUSTERED 
 (
 	[OrderLineId] ASC
@@ -32,9 +49,23 @@ CREATE TABLE [dbo].[OrderLine](
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [dbo].[OrderLine]  WITH CHECK ADD  CONSTRAINT [FK_Order_Product] FOREIGN KEY([ProductId])
+ALTER TABLE [dbo].[OrderLine]  WITH CHECK ADD CONSTRAINT [FK_Order_Product] FOREIGN KEY([ProductId])
 REFERENCES [dbo].[Product] ([ProductId])
 GO
 
+ALTER TABLE [dbo].[OrderHeader]  WITH CHECK ADD CONSTRAINT [FK_Order_Client] FOREIGN KEY([ClientId])
+REFERENCES [dbo].[Client] ([ClientId])
+GO
+
+ALTER TABLE [dbo].[OrderLine]  WITH CHECK ADD CONSTRAINT [FK_Order_OrderId] FOREIGN KEY([OrderId])
+REFERENCES [dbo].[OrderHeader] ([OrderId])
+GO
+
 ALTER TABLE [dbo].[OrderLine] CHECK CONSTRAINT [FK_Order_Product]
+GO
+
+ALTER TABLE [dbo].[OrderHeader] CHECK CONSTRAINT [FK_Order_Client]
+GO
+
+ALTER TABLE [dbo].[OrderLine] CHECK CONSTRAINT [FK_Order_OrderId]
 GO
